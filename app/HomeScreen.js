d6dc3745a77d2ca937722dbac2721ae37e623ffa
Eralp Karaduman <eralpkaraduman@gmail.com>
2017-06-18 03:33:43 +0300

    reverted to objc

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Button,
+  AlertIOS,
+  AppState
 } from 'react-native';
 
 import styles from './styles';
@@ -12,23 +15,68 @@ export default class HomeScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            echoResponse: 'Not Received'
+            microphonePermission: false,
+            microphonePermissionDenied: false,
+
+            speechRecognitionPermission: false,
+            speechRecognitionPermissionDenied: false
         };
     }
 
     componentDidMount() {
-        RNSFSpeechRecognizer.echo("ECHO FROM NATIVE COMPONENT",
-            echoResponse => this.setState(
-                () => ({echoResponse})
-            )
+        this._checkPermissions();
+        AppState.addEventListener('change', this._handleAppStateChange);
+    }
+
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this._handleAppStateChange);
+    }
+
+    _handleAppStateChange = nextAppState => {
+        if (nextAppState === 'active') {
+            this._checkPermissions();
+        }
+    }
+
+    _checkPermissions = () => {
+        RNSFSpeechRecognizer.isMicrophonePermissionGranted(
+            microphonePermission => this.setState(() => ({microphonePermission}))
+        );
+        RNSFSpeechRecognizer.isMicrophonePermissionDenied(
+            microphonePermissionDenied => this.setState(() => ({microphonePermissionDenied}))
+        );
+    }
+
+    _requestMicrophonePermission = () => {
+        if (this.state.microphonePermissionDenied) {
+            AlertIOS.alert(
+                'Microphone Access Was Denied',
+                'We cant turn it on. You\'ll need to go ' +
+                'to "iOS System Settings > This App" to manualy allow access'
+            );
+            return;
+        }
+
+        RNSFSpeechRecognizer.requestMicrophonePermission(
+            () => this._checkPermissions()
         );
     }
 
     render() {
         return (
             <View>
-                <Text>Home Screen</Text>
-                <Text>{`Echo: ${this.state.echoResponse}`}</Text>
+                {!this.state.microphonePermission && (
+                    <Button
+                        onPress={this._requestMicrophonePermission}
+                        title="Allow Microphone Access"
+                    />
+                )}
+                {!this.state.speechRecognitionPermission && (
+                    <Button
+                        onPress={() => AlertIOS.alert('Not implemented yet.')}
+                        title="Allow Speech Recognition"
+                    />
+                )}
             </View>
         );
     }
